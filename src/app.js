57996d0d350f8d39ead19245ede75740fa3e1414
woong <koohw0109@gmail.com>
2023-03-31 10:43:36 +0900

    commit

--- src/app.js
+++ src/app.js
@@ -5,13 +5,13 @@ const app = express();
 app.use("/", express.static("../src"));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/html/index.html");
+  res.sendFile(__dirname + "/html/index.html");
 });
 
 app.get("/login", (req, res) => {
-    res.sendFile(__dirname + "/html/login.html");
+  res.sendFile(__dirname + "/html/login.html");
 });
 
 app.listen(3000, () => {
-    console.log("Example app listening on port 3000!");
+  console.log("Example app listening on port 3000!");
 });
--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,3 +1,3 @@
 function movePage(route) {
-    location.href = route;
+  location.href = route;
 }
