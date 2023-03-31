3f57d4d6a3f21bdaaf1a98641e146879b2aa70a7
woong <koohw0109@gmail.com>
2023-03-31 14:59:05 +0900

    dd

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const app = express();
+const port = 3000;
 
 app.use("/", express.static("../src"));
 
@@ -12,6 +13,6 @@ app.get("/login", (req, res) => {
   res.sendFile(__dirname + "/html/login.html");
 });
 
-app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
 });
