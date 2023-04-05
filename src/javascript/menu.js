e79b1f5fcafca3ee24066142e090a577b9a0c486
igiza1213 <igiza1213@gmail.com>
2023-04-05 18:43:51 +0900

    menubar

--- src/javascript/menu.js
+++ src/javascript/menu.js
@@ -1,13 +1,8 @@
-const bar = document.getElementById("bar").addEventListener("click", floatBar);
-const bar2 = document
-  .getElementById("menubar_f")
-  .addEventListener("click", floatBar);
-
 function floatBar() {
-  let dis = document.getElementById("menubar_f");
-  if (dis.style.display == "none") {
-    dis.style.display = "flex";
-  } else {
-    dis.style.display = "none";
-  }
+    let dis = document.getElementById("menubar_f");
+    if (dis.style.display == "none") {
+        dis.style.display = "flex";
+    } else {
+        dis.style.display = "none";
+    }
 }
