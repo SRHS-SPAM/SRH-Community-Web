f194615916e056a2f42e8f0f6dd5f15bc66e70bf
igiza1213 <igiza1213@gmail.com>
2023-04-09 01:56:14 +0900

    1

---
+++ main/src/components/Arrow.js
@@ -0,0 +1,61 @@
+import Link from "next/link";
+
+export default function Arrow({ location }) {
+    return (
+        <>
+            <Link href={location}>
+                <div className="arrow">
+                    <div className="one" />
+                    <div className="two" />
+                    <div className="three" />
+                </div>
+            </Link>
+            <style jsx>
+                {`
+                    .arrow {
+                        position: absolute;
+                        width: 100px;
+                        height: 100px;
+                        top: 0;
+                        left: 0;
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        scale: 0.8;
+                        cursor: pointer;
+                    }
+
+                    .one {
+                        position: absolute;
+                        width: 40px;
+                        height: 10px;
+                        border-radius: 5px;
+                        background-color: #286246;
+                        rotate: -45deg;
+                        top: 34px;
+                        left: 0;
+                    }
+
+                    .two {
+                        position: absolute;
+                        width: 74px;
+                        height: 10px;
+                        border-radius: 5px;
+                        background-color: #286246;
+                    }
+
+                    .three {
+                        position: absolute;
+                        width: 40px;
+                        height: 10px;
+                        border-radius: 5px;
+                        background-color: #286246;
+                        rotate: 45deg;
+                        bottom: 34px;
+                        left: 0;
+                    }
+                `}
+            </style>
+        </>
+    );
+}
--- main/src/pages/_app.js
+++ main/src/pages/_app.js
@@ -1,5 +1,34 @@
-import '@/styles/globals.css'
+import "@/styles/reset.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <meta
+                    name="description"
+                    content="Seoul Robot HighSchool Community"
+                />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Component {...pageProps} />
+            <style jsx global>{`
+                * {
+                    box-sizing: border-box;
+                }
+
+                body {
+                    font-family: system-ui, -apple-system, BlinkMacSystemFont,
+                        "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell,
+                        "Open Sans", "Helvetica Neue", sans-serif;
+                    background-color: #fffdefd1;
+                    height: 100vh;
+                }
+            `}</style>
+        </>
+    );
 }
