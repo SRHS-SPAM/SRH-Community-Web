28939792f3679e712e794c1c45f249de176b5a3e
igiza1213 <igiza1213@gmail.com>
2023-06-01 15:39:04 +0900

    n

--- new/components/CalendarMain.js
+++ new/components/CalendarMain.js
@@ -1,13 +1,69 @@
-"use client";
-import { useState } from "react";
-import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css"; // css import
+import styles from "./CalendarMain.module.css";
 
 export default function CalendarMain() {
-    const [value, onChange] = useState(new Date());
     return (
         <>
-            <Calendar onChange={onChange} value={value} />
+            <div className={styles.calender_0}>
+                <div className={styles.month_year}>March 2022</div>
+                <div className={styles.day}>
+                    <div className={styles.row}>
+                        <li>SUN</li>
+                        <li className={styles.op}>28</li>
+                        <li>5</li>
+                        <li>12</li>
+                        <li>19</li>
+                        <li>26</li>
+                    </div>
+                    <div className={styles.row}>
+                        <li>MON</li>
+                        <li className={styles.op}>29</li>
+                        <li>6</li>
+                        <li>13</li>
+                        <li>20</li>
+                        <li>27</li>
+                    </div>
+                    <div className={styles.row}>
+                        <li>TUE</li>
+                        <li className={styles.op}>30</li>
+                        <li>7</li>
+                        <li>14</li>
+                        <li>21</li>
+                        <li>28</li>
+                    </div>
+                    <div className={styles.row}>
+                        <li>WED</li>
+                        <li>1</li>
+                        <li>8</li>
+                        <li>15</li>
+                        <li>22</li>
+                        <li>29</li>
+                    </div>
+                    <div className={styles.row}>
+                        <li>THU</li>
+                        <li>2</li>
+                        <li>9</li>
+                        <li>16</li>
+                        <li>23</li>
+                        <li>30</li>
+                    </div>
+                    <div className={styles.row}>
+                        <li>FRI</li>
+                        <li>3</li>
+                        <li>10</li>
+                        <li>17</li>
+                        <li className={styles.today}>24</li>
+                        <li>31</li>
+                    </div>
+                    <div className={styles.row}>
+                        <li>SAT</li>
+                        <li>4</li>
+                        <li>11</li>
+                        <li>18</li>
+                        <li>25</li>
+                        <li className={styles.op}>1</li>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
