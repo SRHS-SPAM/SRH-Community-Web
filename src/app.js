88607e3ffc4b4c19104474a0443c0253dab0ef9a
igiza1213 <igiza1213@gmail.com>
2023-06-04 11:48:17 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,20 +3,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use("/", express.static("../"));
+app.use("/", express.static(__dirname + "public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/html/index.html");
+    res.sendFile(__dirname + "/html/index.html");
 });
 
 app.get("/login", (req, res) => {
-  res.sendFile(__dirname + "/html/login.html");
+    res.sendFile(__dirname + "/html/login.html");
 });
 
 app.get("/signup", (req, res) => {
-  res.sendFile(__dirname + "/html/signup.html");
+    res.sendFile(__dirname + "/html/signup.html");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+    console.log(`Example app listening on port ${port}!`);
 });
